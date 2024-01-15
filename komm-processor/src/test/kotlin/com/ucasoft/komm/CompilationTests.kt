17db734da18d57ef72daee0a1740d38b1c8b1af0
Sergey <AntonovSergeyS@gmail.com>
2024-01-15 14:53:44 +0100

    Improve tests

---
+++ komm-processor/src/test/kotlin/com/ucasoft/komm/CompilationTests.kt
@@ -0,0 +1,82 @@
+package com.ucasoft.komm
+
+import KOMMProcessorProvider
+import com.squareup.kotlinpoet.*
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspWithCompilation
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+import kotlin.reflect.KClass
+
+abstract class CompilationTests {
+
+    @TempDir
+    private lateinit var tempDir: File
+
+    abstract val packageName: String
+
+    fun buildFileSpec(
+        className: String,
+        constructorProperties: Map<String, KClass<*>>,
+        classAnnotations: List<Pair<KClass<out Annotation>, Map<String, List<Any>>>> = emptyList(),
+        properties: Map<String, KClass<*>> = emptyMap()
+    ) = FileSpec
+        .builder(packageName, "$className.kt")
+        .addType(
+            TypeSpec
+                .classBuilder(className)
+                .addModifiers(KModifier.DATA)
+                .primaryConstructor(
+                    FunSpec
+                        .constructorBuilder()
+                        .apply {
+                            constructorProperties.forEach {
+                                addParameter(it.key, it.value)
+                            }
+                        }
+                        .build())
+                .apply {
+                    classAnnotations.forEach {
+                        addAnnotation(
+                            AnnotationSpec
+                                .builder(it.first)
+                                .apply {
+                                    for (member in it.second) {
+                                        addMember(member.key, *member.value.toTypedArray())
+                                    }
+                                }
+                                .build()
+                        )
+                    }
+                    constructorProperties.forEach {
+                        addProperty(
+                            PropertySpec
+                                .builder(it.key, it.value)
+                                .initializer(it.key)
+                                .build()
+                        )
+                    }
+                    properties.forEach {
+                        addProperty(
+                            PropertySpec
+                                .builder(it.key, it.value)
+                                .initializer("%S", "")
+                                .mutable()
+                                .build()
+                        )
+                    }
+                }
+                .build()
+        )
+        .build()
+
+    fun generate(vararg fileSpec: FileSpec) = KotlinCompilation().apply {
+        inheritClassPath = true
+        kspWithCompilation = true
+        sources = fileSpec.map { SourceFile.kotlin(it.name, it.toString()) }
+        symbolProcessorProviders = listOf(KOMMProcessorProvider())
+        workingDir = tempDir
+    }.compile()
+}
--- komm-processor/src/test/kotlin/com/ucasoft/komm/processor/ProcessorTests.kt
+++ komm-processor/src/test/kotlin/com/ucasoft/komm/processor/ProcessorTests.kt
@@ -6,6 +6,7 @@ import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.kspWithCompilation
 import com.tschuchort.compiletesting.symbolProcessorProviders
+import com.ucasoft.komm.CompilationTests
 import com.ucasoft.komm.annotations.KOMMMap
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.collections.shouldBeSingleton
@@ -13,6 +14,7 @@ import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.reflection.shouldHaveMemberProperty
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import org.junit.jupiter.params.ParameterizedTest
@@ -21,12 +23,9 @@ import java.io.File
 import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.KClass
 
-internal class ProcessorTests {
+internal class ProcessorTests : CompilationTests() {
 
-    @TempDir
-    private lateinit var tempDir: File
-
-    private val packageName = "com.text.model"
+    override val packageName = "com.text.model"
 
     private val properties = mapOf(
         "id" to (Int::class to Int::class),
@@ -35,95 +34,20 @@ internal class ProcessorTests {
         "intToString" to (Int::class to String::class)
     )
 
-    private val sourceObject = FileSpec
-        .builder(packageName, "SourceModel.kt")
-        .addType(
-            TypeSpec
-                .classBuilder("SourceObject")
-                .addModifiers(KModifier.DATA)
-                .primaryConstructor(FunSpec
-                    .constructorBuilder()
-                    .apply {
-                        properties.forEach {
-                            addParameter(it.key, it.value.first)
-                        }
-                    }
-                    .build())
-                .apply {
-                    properties.forEach {
-                        addProperty(
-                            PropertySpec
-                                .builder(it.key, it.value.first)
-                                .initializer(it.key)
-                                .build()
-                        )
-                    }
-                }
-                .build()
-        )
-        .build()
+    private val sourceObject = buildFileSpec("SourceObject", properties.map { it.key to it.value.first }.toMap())
 
     private val sourceObjectClassName = sourceObject.members.filterIsInstance<TypeSpec>().first().name
 
-    private val destinationObject = FileSpec
-        .builder(packageName, "DestinationModel.kt")
-        .addType(
-            TypeSpec
-                .classBuilder("DestinationObject")
-                .addModifiers(KModifier.DATA)
-                .addAnnotation(
-                    AnnotationSpec
-                        .builder(KOMMMap::class)
-                        .addMember("from = %L", "$sourceObjectClassName::class")
-                        .build()
-                )
-                .primaryConstructor(
-                    FunSpec
-                        .constructorBuilder()
-                        .apply {
-                            properties.forEach {
-                                if (it.key != "intToString") {
-                                    addParameter(it.key, it.value.second)
-                                }
-                            }
-                        }
-                        .build()
-                )
-                .apply {
-                    properties.forEach {
-                        if (it.key != "intToString") {
-                            addProperty(
-                                PropertySpec
-                                    .builder(it.key, it.value.second)
-                                    .initializer(it.key)
-                                    .build()
-                            )
-                        }
-                    }
-                }
-                .addProperty(
-                    PropertySpec
-                        .builder("intToString", String::class)
-                        .initializer("%S", "")
-                        .mutable()
-                        .build()
-                )
-                .build()
-        )
-        .build()
+    private val destinationObject = buildFileSpec(
+        "DestinationObject",
+        properties.filter { it.key != "intToString"}.map { it.key to it.value.second }.toMap(),
+        listOf(KOMMMap::class to mapOf("from = %L" to listOf("$sourceObjectClassName::class"))),
+        properties.filter { it.key == "intToString" }.map { it.key to it.value.second }.toMap()
+    )
 
     @Test
     fun checkSuccessGeneration() {
-        val generated = KotlinCompilation().apply {
-            inheritClassPath = true
-            kspWithCompilation = true
-            sources = listOf(
-                SourceFile.kotlin(sourceObject.name, sourceObject.toString()),
-                SourceFile.kotlin(destinationObject.name, destinationObject.toString())
-            )
-            symbolProcessorProviders = listOf(KOMMProcessorProvider())
-            workingDir = tempDir
-        }.compile()
+        val generated = generate(sourceObject, destinationObject)
 
         generated.exitCode.shouldBe(KotlinCompilation.ExitCode.OK)
 
@@ -135,16 +59,7 @@ internal class ProcessorTests {
 
     @Test
     fun checkObjectMapping() {
-        val generated = KotlinCompilation().apply {
-            inheritClassPath = true
-            kspWithCompilation = true
-            sources = listOf(
-                SourceFile.kotlin(sourceObject.name, sourceObject.toString()),
-                SourceFile.kotlin(destinationObject.name, destinationObject.toString())
-            )
-            symbolProcessorProviders = listOf(KOMMProcessorProvider())
-            workingDir = tempDir
-        }.compile()
+        val generated = generate(sourceObject, destinationObject)
 
         generated.exitCode.shouldBe(KotlinCompilation.ExitCode.OK)
 
@@ -159,7 +74,7 @@ internal class ProcessorTests {
         val mappingMethod = mappingClass.declaredMethods.first()
         val sourceClass = generated.classLoader.loadClass("$packageName.$sourceObjectClassName")
         val params = properties.values.map { it.first }
-        val sourceInstance = sourceClass.constructors.first().newInstance(params[0], params[1], params[2], params[3])
+        val sourceInstance = sourceClass.constructors.first().newInstance(*params.toTypedArray())
         val destinationInstance = mappingMethod.invoke(null, sourceInstance)
 
         destinationInstance.shouldNotBeNull()
@@ -173,16 +88,7 @@ internal class ProcessorTests {
     @ParameterizedTest
     @CsvSource(value = ["123,String,456.0,789"])
     fun checkObjectMappingCallFailed(id: Int, strToInt: String, doubleToInt: Double, intToString: Int) {
-        val generated = KotlinCompilation().apply {
-            inheritClassPath = true
-            kspWithCompilation = true
-            sources = listOf(
-                SourceFile.kotlin(sourceObject.name, sourceObject.toString()),
-                SourceFile.kotlin(destinationObject.name, destinationObject.toString())
-            )
-            symbolProcessorProviders = listOf(KOMMProcessorProvider())
-            workingDir = tempDir
-        }.compile()
+        val generated = generate(sourceObject, destinationObject)
 
         generated.exitCode.shouldBe(KotlinCompilation.ExitCode.OK)
 
