210deb631fed817f3adbfb185142b137fa3ff2f4
Sergey <AntonovSergeyS@gmail.com>
2024-01-20 11:33:27 +0100

    Extract MapDefault separate file

---
+++ komm-annotations/src/main/kotlin/com/ucasoft/komm/annotations/MapDefault.kt
@@ -0,0 +1,9 @@
+package com.ucasoft.komm.annotations
+
+import com.ucasoft.komm.abstractions.KOMMResolver
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.SOURCE)
+@Repeatable
+annotation class MapDefault<R: KOMMResolver<*, *>>(val resolver: KClass<R>, val from: Array<KClass<*>> = [])
--- komm-annotations/src/main/kotlin/com/ucasoft/komm/annotations/MapFrom.kt
+++ komm-annotations/src/main/kotlin/com/ucasoft/komm/annotations/MapFrom.kt
@@ -1,14 +1,8 @@
 package com.ucasoft.komm.annotations
 
-import com.ucasoft.komm.abstractions.KOMMResolver
 import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.SOURCE)
 @Repeatable
 annotation class MapFrom(val name: String, val from: Array<KClass<*>> = [])
-
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.SOURCE)
-@Repeatable
-annotation class MapDefault<R: KOMMResolver<*, *>>(val resolver: KClass<R>, val from: Array<KClass<*>> = [])
