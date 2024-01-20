5461d8b2a155872d169c13e51bee277f252f726b
Sergey <AntonovSergeyS@gmail.com>
2024-01-20 11:33:45 +0100

    Update Libraries

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ pluginManagement {
         plugins {
             val kotlinVersion = "1.9.22"
             kotlin("multiplatform") version kotlinVersion apply false
-            id("com.google.devtools.ksp") version "$kotlinVersion-1.0.16" apply false
+            id("com.google.devtools.ksp") version "$kotlinVersion-1.0.17" apply false
         }
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "komm"
