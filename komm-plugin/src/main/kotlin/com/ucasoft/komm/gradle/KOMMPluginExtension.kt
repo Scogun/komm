b0a43eb486ce9a4972af9991fbbb6b068f4fa41d
Sergey <AntonovSergeyS@gmail.com>
2024-02-19 21:44:46 +0100

    Try to make project build

--- komm-plugin/src/main/kotlin/com/ucasoft/komm/gradle/KOMMPluginExtension.kt
+++ komm-plugin/src/main/kotlin/com/ucasoft/komm/gradle/KOMMPluginExtension.kt
@@ -1,2 +1,17 @@
-abstract class KOMMPluginExtension {
+package com.ucasoft.komm.gradle
+
+import org.gradle.api.Action
+import org.gradle.api.Project
+
+abstract class KOMMPluginExtension(private val project: Project) {
+
+    fun apply(action: Action<KOMMPluginConfig>) {
+        val config = KOMMPluginConfig()
+        action.execute(config)
+        apply(config)
+    }
+
+    fun apply(config: KOMMPluginConfig) {
+
+    }
 }
