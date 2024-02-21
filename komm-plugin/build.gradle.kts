13be047b0d1c223c56eb4f7b66312bd9e28487a3
Sergey <AntonovSergeyS@gmail.com>
2024-02-21 16:40:41 +0100

    Add Test

--- komm-plugin/build.gradle.kts
+++ komm-plugin/build.gradle.kts
@@ -5,3 +5,23 @@ plugins {
     id("com.google.devtools.ksp") apply false
     alias(libs.plugins.kotlinx.kover) apply false
 }
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+}
+
+gradlePlugin {
+    plugins {
+        create("komm") {
+            id = "com.ucasoft.komm"
+            implementationClass = "com.ucasoft.komm.gradle.KOMMPlugin"
+        }
+    }
+}
---
+++ komm-plugin/src/test/kotlin/com/ucasoft/komm/gradle/KOMMPluginTests.kt
@@ -0,0 +1,52 @@
+package com.ucasoft.komm.gradle
+
+import io.kotest.matchers.string.shouldNotBeEmpty
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+internal class KOMMPluginTests {
+
+    @field:TempDir
+    private lateinit var testProjectDir: File
+
+    private lateinit var settingsFile: File
+
+    private lateinit var buildFile: File
+
+    @BeforeEach
+    fun setup() {
+        settingsFile = File(testProjectDir, "settings.gradle.kts")
+        settingsFile.appendText("""
+           rootProject.name = "test" 
+        """)
+        buildFile = File(testProjectDir, "build.gradle.kts")
+    }
+
+    @Test
+    fun `test plugin is applied`() {
+        buildFile.appendText("""
+            plugins {
+                kotlin("jvm") version "1.9.22"
+                id("com.ucasoft.komm")
+            }          
+        """)
+
+        val result = runTask(":tasks")
+
+        result.output.shouldNotBeEmpty()
+    }
+
+    private fun runTask(task: String, params: Map<String, Any> = emptyMap(), arguments: List<String> = emptyList()): BuildResult {
+        return GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withArguments(mutableListOf(task).also { it.addAll(params.map { p -> "-P${p.key}=${p.value}" }) }
+                .also { it.addAll(arguments) }
+                .also { it.add("--stacktrace") })
+            .withPluginClasspath()
+            .build()
+    }
+}
