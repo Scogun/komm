2342c0ecca1286cddb26a8c30006a671c95d0409
Sergey <AntonovSergeyS@gmail.com>
2024-03-18 14:49:02 +0100

    Remove smell

--- komm-processor/src/main/kotlin/com/ucasoft/komm/processor/extensions/KSAnnotationExtensions.kt
+++ komm-processor/src/main/kotlin/com/ucasoft/komm/processor/extensions/KSAnnotationExtensions.kt
@@ -5,7 +5,9 @@ import kotlin.reflect.KClass
 
 internal inline fun <reified T: Any> KSAnnotation.getConfigValue(key: String) : T {
     val value = this.arguments.first { it.name?.asString() == key }.value.toString()
-    return castConfigValue(value, T::class) as T
+    val castValue = castConfigValue(value, T::class)
+    require(castValue is T) { "Value $value cannot be cast to ${T::class}" }
+    return castValue
 }
 
 internal inline fun <reified T : Any> castConfigValue(value: String, `class`: KClass<T>) = when(`class`) {
