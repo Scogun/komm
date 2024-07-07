884a079fae1808d68b102b9bf0ea0df959026747
Sergey <AntonovSergeyS@gmail.com>
2024-07-07 18:28:07 +0200

    Add Exposed Plugin

---
+++ komm-plugins-exposed/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("multiplatform")
+    alias(libs.plugins.kotlinx.kover)
+    id("publish")
+    id("com.google.devtools.ksp") apply false
+}
+
+kotlin {
+    jvm {
+        tasks.withType<Test> {
+            useJUnitPlatform()
+            reports {
+                junitXml.required.set(true)
+            }
+        }
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":komm-plugins-core"))
+                implementation(libs.exposed.core)
+                implementation(libs.kotlin.poet.ksp)
+            }
+            kotlin.srcDir("src/main/kotlin")
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(libs.junit.jupiter)
+                implementation(libs.kotest.assertions)
+                implementation(libs.mockk)
+                implementation(libs.kotlin.poet.ksp)
+                implementation(kotlin("reflect"))
+            }
+            kotlin.srcDir("src/test/kotlin")
+        }
+    }
+}
+
+libraryData {
+    name.set("KOMM Plugins Exposed")
+    description.set("Plugins to map Exposed ResultRow to DTO Object for Kotlin Object Multiplatform Mapper Plugins")
+}
---
+++ komm-plugins-exposed/src/main/kotlin/com/ucasoft/komm/plugins/exposed/ExposedPlugin.kt
@@ -0,0 +1,48 @@
+package com.ucasoft.komm.plugins.exposed
+
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSDeclaration
+import com.google.devtools.ksp.symbol.KSPropertyDeclaration
+import com.google.devtools.ksp.symbol.KSType
+import com.squareup.kotlinpoet.asClassName
+import com.squareup.kotlinpoet.asTypeName
+import com.squareup.kotlinpoet.ksp.toClassName
+import com.squareup.kotlinpoet.ksp.toTypeName
+import com.ucasoft.komm.plugins.KOMMTypePlugin
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.Table
+
+class ExposedPlugin: KOMMTypePlugin {
+
+    override fun forType(sourceType: KSType) = (sourceType.declaration as KSClassDeclaration).superTypes.any { it.toTypeName() == Table::class.asTypeName() }
+
+    override fun type(sourceType: KSType) = ResultRow::class
+
+    override fun forCast(sourceType: KSType, destinationType: KSType) = sourceType.toClassName() == Column::class.asClassName()
+
+    override fun cast(
+        sourceName: String,
+        sourceType: KSType,
+        destinationProperty: KSPropertyDeclaration,
+        destinationType: KSType
+    ): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun cast(
+        sourceProperty: KSDeclaration,
+        sourceName: String,
+        sourceType: KSType,
+        destinationProperty: KSPropertyDeclaration,
+        destinationType: KSType
+    ): String {
+        val result = StringBuilder("this[")
+        val sourceObject = sourceProperty.parentDeclaration!!.simpleName.asString()
+        result.append("$sourceObject.$sourceName]")
+        if (destinationType.toTypeName() != sourceType.arguments.first().toTypeName()) {
+            result.append(".to${destinationProperty.type}()")
+        }
+        return result.toString()
+    }
+}
