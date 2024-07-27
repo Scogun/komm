2b3c84f96a8d4b94fcbffaea95bbca7b570d0f59
Sergey <AntonovSergeyS@gmail.com>
2024-07-27 10:29:54 +0200

    Remove old annotation

--- komm-annotations/src/main/kotlin/com/ucasoft/komm/annotations/MapFrom.kt
+++
@@ -1,9 +0,0 @@
-package com.ucasoft.komm.annotations
-
-import kotlin.reflect.KClass
-
-@Deprecated("Use MapName instead", ReplaceWith("MapName"))
-@Target(AnnotationTarget.PROPERTY)
-@Retention(AnnotationRetention.SOURCE)
-@Repeatable
-annotation class MapFrom(val name: String, val from: Array<KClass<*>> = [])
