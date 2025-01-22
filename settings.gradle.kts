5d81d7e53ff58ef3ba48e67885d753348ef97732
Sergey <AntonovSergeyS@gmail.com>
2025-01-22 14:10:31 +0100

    Update Plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ pluginManagement {
         plugins {
             val kotlinVersion = "2.1.0"
             kotlin("multiplatform") version kotlinVersion apply false
-            id("com.google.devtools.ksp") version "$kotlinVersion-1.0.25" apply false
+            id("com.google.devtools.ksp") version "$kotlinVersion-1.0.29" apply false
         }
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
 rootProject.name = "komm"
