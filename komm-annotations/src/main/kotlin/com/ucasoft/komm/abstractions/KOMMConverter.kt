2353425cbfb7837959174c2390d2607bcdaca8ed
Sergey Antonov <AntonovSergeyS@gmail.com>
2025-05-01 11:57:49 +0200

    Fix issue

--- komm-annotations/src/main/kotlin/com/ucasoft/komm/abstractions/KOMMConverter.kt
+++ komm-annotations/src/main/kotlin/com/ucasoft/komm/abstractions/KOMMConverter.kt
@@ -1,7 +1,5 @@
 package com.ucasoft.komm.abstractions
 
-import kotlin.reflect.KClass
-
 abstract class KOMMConverter<S, SM, D, DM>(protected val source: S) {
 
     abstract fun convert(sourceMember: SM) : DM
