7ac83991f81a14a96cae03a5d251035d8abf663d
Sergey <AntonovSergeyS@gmail.com>
2025-05-15 21:49:37 +0200

    Extend Lucide

--- komm-website/src/jsMain/kotlin/com/ucasoft/wrappers/lucide/Lucide.kt
+++ komm-website/src/jsMain/kotlin/com/ucasoft/wrappers/lucide/Lucide.kt
@@ -4,8 +4,14 @@ package com.ucasoft.wrappers.lucide
 
 import react.FC
 
+@JsName("Database")
+external val Database: FC<LucideProps>
+
 @JsName("Github")
 external val GitHub: FC<LucideProps>
+
+@JsName("Layers")
+external val Layers: FC<LucideProps>
 
 @JsName("Library")
 external val Library: FC<LucideProps>
@@ -13,5 +19,17 @@ external val Library: FC<LucideProps>
 @JsName("Menu")
 external val Menu: FC<LucideProps>
 
+@JsName("SlidersVertical")
+external val SlidersVertical: FC<LucideProps>
+
+@JsName("SquareTerminal")
+external val SquareTerminal: FC<LucideProps>
+
+@JsName("Shield")
+external val Shield: FC<LucideProps>
+
 @JsName("X")
 external val X: FC<LucideProps>
+
+@JsName("Zap")
+external val Zap: FC<LucideProps>
